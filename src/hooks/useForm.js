d7ebc2c6b06d5b2bb9c3630751ff4358bbaddb8b
Joey_M_Brown <joey.ma.brown@gmail.com>
2021-08-06 14:02:36 -0400

    Added axios call in PlantList, Moved local stateful logic to custom hook in useForm.js

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,34 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react';
+
+
+const useLocalStorage = (key, initialValue) => {
+    const [value, setValue] = useState(() => {
+        const item = localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue; 
+    })
+
+    localStorage.setItem(key, JSON.stringify(value));
+
+    const updateLocalStorageValue = value => {
+        setValue(value);
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    return [value, updateLocalStorageValue];
+}
+
+
+function useForm (initialValue) {
+   const [values, setValues] = useLocalStorage("form", initialValue) //values now = either initial values, or what was returned from localstorage.  setValues now = updating state and localStorage.
+
+    const handleChanges = e => {
+        const {name, value} = e.target;
+
+        setValues({...values, [name]: value});
+    };
+
+   return [values, handleChanges];
+}
+
+export default useForm;
